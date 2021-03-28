60ea2bb4fbcf3b990441aff6ad1f4d879bfa0840
judy <ljd69154@liangjundi.cn>
2021-03-29 05:39:38 +0800

    add - reformat the py file

--- ai-hyperion/src/plugins/colorful/__init__.py
+++ ai-hyperion/src/plugins/colorful/__init__.py
@@ -1,6 +1,12 @@
 # import nonebot
-from nonebot import get_driver
+import os
+import random
 
+from nonebot import get_driver
+from nonebot.adapters.cqhttp import Bot, MessageEvent
+from nonebot.plugin import on_regex
+
+from . import data_source
 from .config import Config
 
 global_config = get_driver().config
@@ -14,13 +20,6 @@ config = Config(**global_config.dict())
 # def some_function():
 #     pass
 
-from nonebot.plugin import on_regex, on_command
-from nonebot.adapters.cqhttp import Bot, MessageEvent
-from nonebot.adapters.cqhttp import GroupMessageEvent
-
-from . import data_source
-
-import os, random
 
 REG_COLORFUL = '^(色来|色图|涩图|来点涩图|来点色图)$'
 DIR_IMAGE = '/src/plugins/colorful/image/creep/'
@@ -28,10 +27,11 @@ PROT_FILE = 'file:///'
 
 colorful = on_regex(REG_COLORFUL)
 
+
 @colorful.handle()
 async def _colorful(bot: Bot, event: MessageEvent):
     switch_flag = False
-    if switch_flag == True:
+    if switch_flag is True:
         dicts = data_source.get_colorful()['data'][0]
         url = dicts['url']
         pid = 'PID: ' + str(dicts['pid']) + '\n'
@@ -42,17 +42,17 @@ async def _colorful(bot: Bot, event: MessageEvent):
             'data': {
                 'text': pid
             }
-        },{
+        }, {
             'type': 'text',
             'data': {
                 'text': title
             }
-        },{
+        }, {
             'type': 'text',
             'data': {
                 'text': author
             }
-        },{
+        }, {
             'type': 'image',
             'data': {
                 'file': url
@@ -65,16 +65,16 @@ async def _colorful(bot: Bot, event: MessageEvent):
         img_path = PROT_FILE + img_dir + luck_dog
         lsp = event.get_user_id()
         result = [{
-                'type': 'image',
-                'data': {
+            'type': 'image',
+            'data': {
                 'file': img_path
             }
-        },{
+        }, {
             'type': 'text',
             'data': {
                 'text': '不许色！'
             }
-        },{
+        }, {
             'type': 'at',
             'data': {
                 'qq': lsp
