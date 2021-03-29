cfb58ad5c7b889ecc5f66e7317468a0825d50cad
judy <ljd69154@liangjundi.cn>
2021-03-30 07:57:07 +0800

    update - a little things

--- ai-hyperion/src/plugins/utils/interface/rss_news.py
+++ ai-hyperion/src/plugins/utils/interface/rss_news.py
@@ -1,19 +1,21 @@
+from typing import List
+
 import httpx
 import json
 import xmltodict
 import time
 
 
-def get_news(rss_source: str, quantity: int) -> list:
+def get_news(target: str, quantity: int) -> dict:
     """
 
-    :param rss_source: rss url, must fit rss v2.0 standard
+    :param target: rss target, the url must fit rss v2.0 standard
     :param quantity: to control how many news wants to return
     :return: the top 5 news from rss_source
     """
     # url = 'https://a.jiemian.com/index.php?m=article&a=rss'
 
-    rss_source = rss_sources[rss_source]
+    # rss_source = rss_sources[rss_source]
 
     proxies = {
             # 部署到服务器或者容器里面之后 需要修改为对应的
@@ -22,43 +24,47 @@ def get_news(rss_source: str, quantity: int) -> list:
         }
 
     try:
-        r = httpx.get(rss_source, proxies=proxies)
+        r = httpx.get(rss_sources[target], proxies=proxies)
     except httpx.RequestError:
         raise Exception('Interface Error / 接口异常')
     else:
         xml = xmltodict.parse(r.text)
         payload = json.loads(json.dumps(xml))
 
-        msg = []
-
+        msg = {}
+        item = []
         for i in range(quantity):
-            msg.append(payload['rss']['channel']['item'][i])
-        # print(msg)
+            item.append(payload['rss']['channel']['item'][i])
+
+        msg['item'] = item
+        msg['target'] = target
         return msg
 
 
-def construct_string(msg: dict, target: str) -> str:
+def construct_string(msg: dict) -> str:
     """
 
+    :param target: 【给我写清楚这个】
     :param msg:  msg is a dict from upstream method
     :return: the message that will forward to QQ
     """
 
     # init variable
+    target = msg['target']
     date = time.strftime('%m月%d日', time.localtime())
     ret = date + ' ' + target + '\n\n'
     num = 0
 
     # print(type(date))
 
-    for item in msg:
+    for item in msg['item']:
         title = item['title']
-        link = item['link']
+        # link = item['link']
         num_hex = '0x{:02X}'.format(num)
-        ret += f'{num_hex}' + '. ' + f'{title}' + '\n' + '\n'
+        ret += f'{num_hex}' + '. ' + f'{title}' + '\n\n'
         num += 1
 
-    return ret
+    return ret[:-2]
 
 
 rss_sources = {
@@ -68,6 +74,6 @@ rss_sources = {
 
 
 if __name__ == '__main__':
-    msg = get_news('https://a.jiemian.com/index.php?m=article&a=rss', 5)
+    msg = get_news('药闻', 5)
     print(construct_string(msg))
 
