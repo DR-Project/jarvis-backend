282dad57ad7c09d65d79e4540fbd17f0f037b338
judy <ljd69154@liangjundi.cn>
2021-03-30 10:25:58 +0800

    update - change the base in the ret string

--- ai-hyperion/src/plugins/utils/interface/crypto_coin.py
+++ ai-hyperion/src/plugins/utils/interface/crypto_coin.py
@@ -48,10 +48,14 @@ def construct_string(msg: dict) -> str:
     product_id = msg['product_id']
     open_utc8 = float(msg['open_utc8'])
     change_percent = round((price - open_utc8) / price * 100, 2)
-    coin = product_id.split('-')[0]
-    print(change_percent)
 
-    ret = '现在' + f'{coin}' + '的价格是1 ' + f'{coin}' + ' = ' + f'{price}' + ' USDT，对比今天开盘价涨幅为 ' + f'{change_percent}' + '%。 '
+    instrument_id = product_id.split('-')
+    coin = instrument_id[0]
+    base = instrument_id[1]
+    # print(change_percent)
+
+    ret = '现在' + f'{coin}' + '的价格是1 ' + f'{coin}' + ' = ' + f'{price}' + ' ' + f'{base}' + '，对比今天开盘价涨幅为 ' \
+          + f'{change_percent}' + '%。 '
     # abandoned
     # ret = '现在BTC单位价格为 ' f'{price}' + ' USDT，折合美元价格为 ' + f'{usd_price}' + ' USD 。'
 
