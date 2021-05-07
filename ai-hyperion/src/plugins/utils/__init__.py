d667b2e77770eee872ceb8b924b99a6ee28ada08
judy <ljd69154@liangjundi.cn>
2021-05-08 03:25:45 +0800

    update => update a login successful message

--- ai-hyperion/src/plugins/utils/__init__.py
+++ ai-hyperion/src/plugins/utils/__init__.py
@@ -8,22 +8,19 @@ import re
 
 from . import data_source
 
-
 global_config = get_driver().config
 config = Config(**global_config.dict())
-
 
 # Constant List
 
 REG_TRAFFIC = '^(查流量|魔法|Magic|magic|cxll|CXLL)$'
 REG_COIN = '^(BTC|EOS|BTG|ADA|DOGE|LTC|ETH|' + \
-            'BCH|BSV|DOT|ATOM|UNI|ZEC|SUSHI|DASH|OKB|OKT)$'
+           'BCH|BSV|DOT|ATOM|UNI|ZEC|SUSHI|DASH|OKB|OKT)$'
 REG_NEWS = '^(药闻|热搜|TESTNEWS)$'
 REG_WEATHER = '^.*(天气)$'
 REG_DDL = '^(DDL|ddl)$'
 EREG_COIN = 'ECOIN'
 COVID_VACC = 'COVID'
-
 
 # Register Event
 
@@ -34,7 +31,6 @@ weather = on_regex(REG_WEATHER)
 ass_ddl = on_regex(REG_DDL)
 exp_cryptocoin = on_command(EREG_COIN)
 covid_vacc = on_regex(COVID_VACC)
-
 
 ''' >>>>>> Core Function for Utils <<<<<< '''
 
@@ -65,10 +61,12 @@ async def _weather(bot: Bot, event: MessageEvent):
     ret = data_source.weather_get(target)
     await bot.send(event, ret, at_sender=False)
 
+
 @ass_ddl.handle()
 async def _ass_ddl(bot: Bot, event: MessageEvent):
     ret = data_source.ddl_get()
     await bot.send(event, ret, at_sender=False)
+
 
 @covid_vacc.handle()
 async def _covid_vacc(bot: Bot, event: MessageEvent):
@@ -84,3 +82,15 @@ async def _exp_cryptocoin(bot: Bot, event: MessageEvent):
     instrument_id = event.get_plaintext().upper()
     ret = data_source.coin_exp_get_price(instrument_id)
     await bot.send(event, ret, at_sender=False)
+
+# 你下次测试一下这段代码，这块我不熟
+'''driver = get_driver()
+
+
+@driver.on_startup
+async def do_something():
+
+    bot = Bot(driver, 'websocket', config, '***')
+    group_id = ***
+    msg = 'Jarvis already back online, Sir.'
+    await bot.send_group_msg(group_id=group_id, message=msg, auto_escape=True)'''
