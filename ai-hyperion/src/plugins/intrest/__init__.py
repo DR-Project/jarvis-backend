508df8d3d2b6286283bb105738c4156658801ec8
judy <ljd69154@liangjundi.cn>
2021-09-26 19:56:01 +0800

    [DEL] remove debug message

--- ai-hyperion/src/plugins/intrest/__init__.py
+++ ai-hyperion/src/plugins/intrest/__init__.py
@@ -85,7 +85,7 @@ async def _roll_ssr(bot: Bot):
         message = Message({
             'type': 'text',
             'data': {
-                'text': 'SSR小游戏已上线，可以发「十连丢人」或者「单抽」 {{debug: @%s}}' % ssr_id
+                'text': 'SSR小游戏已上线，可以发「十连丢人」或者「单抽」'
             }
         })
         logger.info('群[group_id=%d]的SSR已经更新，新的SSR是[qq=%d]' % (group, ssr_id))
